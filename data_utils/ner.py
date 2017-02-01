1fd13e6298d278d9ca3f269df80818595a5f4122
Vincent Porte <groupsforums@free.fr>
2017-02-01 23:57:01 +0100

    pset2 part 2 : Deep Networks for Named Entity Recognition
    q2_NER.py
    + python 2 to python 3 patches (data_utils/ner.py , data_utils/utils.py)
    
    Epoch 5
    Training loss: 0.120153710246086125371024608612
    Training acc: 0.973426120095668
    Validation loss: 0.19072464108467102
    [[42239    85   103   234    98]
    [  137  1814    15    87    41]
    [  262    23   920    47    16]
    [  514   112    31  1349    86]
    [  468    31     5    77  2568]]
    Tag: O - P 0.9683 / R 0.9878
    Tag: LOC - P 0.8785 / R 0.8663
    Tag: MISC - P 0.8566 / R 0.7256
    Tag: ORG - P 0.7520 / R 0.6448
    Tag: PER - P 0.9142 / R 0.8155
    Total time: 16.569891929626465
    Epoch 6
    Training loss: 0.115107297897338870729789733887
    Training acc: 0.9740940276297632
    Validation loss: 0.1912369430065155

---
+++ data_utils/ner.py
@@ -0,0 +1,27 @@
+##
+# Utility functions for NER assignment
+# Assigment 2, part 1 for CS224D
+##
+
+#from utils import invert_dict
+#python 3.5
+from data_utils.utils import invert_dict
+from numpy import *
+
+#def invert_dict(d):
+#    return {v:k for k,v in d.iteritems()}
+
+def load_wv(vocabfile, wvfile):
+    wv = loadtxt(wvfile, dtype=float)
+    with open(vocabfile) as fd:
+        words = [line.strip() for line in fd]
+    num_to_word = dict(enumerate(words))
+    word_to_num = invert_dict(num_to_word)
+    return wv, word_to_num, num_to_word
+
+
+def save_predictions(y, filename):
+    """Save predictions, one per line."""
+    with open(filename, 'w') as fd:
+        fd.write("\n".join(map(str, y)))
+        fd.write("\n")
